7383ed771fc34b8051266c751d063e9cce4b2b28
Gopi Kommavarapu <gopikommavarapu98@gmail.com>
2022-03-09 12:36:19 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Buttons from "./Button"
+import Functional from "./Functional"
+import Clsss from "./clsss"
+
+
+
+const App= () => {
+   return (
+       <>
+       <Buttons />
+       <div className="container1">
+        <Functional />
+        <Clsss />
+       </div>
+       
+       </>
+   )
+}
+export default App
